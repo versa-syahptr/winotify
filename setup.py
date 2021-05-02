9b49271f5666891cdc716c1f10ee29a877e95109
versa-syahptr <versa1220@gmail.com>
2021-05-02 17:33:29 +0700

    fix unfilled launch param and improve the PS script

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='winotify',
-    version='1.0.0',
+    version='1.0.1',
     py_modules=["winotify"],
     url='https://github.com/versa-syahptr/winotify',
     license='MIT',
